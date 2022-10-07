3bb78ab838d32bddbc482e16d44b96f1e9e5a8b0
JJ <jj@Jays-MacBook-Pro.local>
2022-10-06 23:53:58 -0400

    fix bugs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+#CDK for eks cluster deployed in private subnet
+
+#Reference/Base tempalte
+https://github.com/aws-samples/aws-cdk-examples/tree/master/typescript/eks/cluster
+
 #Prerequisites
 ```
 npm install -g typescript
--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ class EKSCluster extends cdk.Stack {
 
     const eksCluster = new eks.Cluster(this, 'Cluster', {
       vpc: vpc,
-      defaultCapacity: 0,  // we want to manage capacity our selves
+      defaultCapacity: 1,  // we want to manage capacity our selves
       version: eks.KubernetesVersion.V1_21,
     });
 
@@ -26,7 +26,7 @@ class EKSCluster extends cdk.Stack {
       role: workerRole,
       minCapacity: 1,
       maxCapacity: 10,
-      instanceType: new ec2.InstanceType('t3.medium'),
+      instanceType: new ec2.InstanceType('t3.nano'),
       machineImage: new eks.EksOptimizedImage({
         kubernetesVersion: '1.21',
         nodeType: eks.NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
